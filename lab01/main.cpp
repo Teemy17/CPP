493332cd1acfc9ef2b2b8c48772e1279867f0ec1
Pannawat Yorkhant <113030120+Teemy17@users.noreply.github.com>
2023-12-12 23:05:13 +0700

    initial commit

---
+++ lab01/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world" << endl;
+    cout << "This is my first C++ program" << endl;
+    cout << "Good bye" << endl;
+}
+
