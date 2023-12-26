ba5b6560705f44a7d118a410d00f7c9f97612fc6
Pannawat Yorkhant <113030120+Teemy17@users.noreply.github.com>
2023-12-26 19:43:11 +0700

    .

--- lab01/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello world" << endl;
-    cout << "This is my first C++ program" << endl;
-    cout << "Good bye" << endl;
-}
-
