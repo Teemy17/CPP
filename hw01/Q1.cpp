84acddb04ba5df1b30a288ce4a92405dea336a7b
Pannawat Yorkhant <Teemy17@gmail.com>
2024-02-17 19:32:52 +0700

    add hw1

--- hw01/Q1.cpp
+++ hw01/Q1.cpp
@@ -1,117 +1,116 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <thread>
+
+using namespace std;
 
 const int SIZE = 15;
+const double WALL_CHANCE = 0.2;
 
-enum CellType {
-    WALL,
-    PATH,
-    ENTRY,
-    EXIT
+struct Cell
+{
+    bool visited = false;
+    char display = '#';
 };
 
-struct Cell {
-    CellType type;
-    bool visited;
-};
+bool IsValid(const vector<vector<Cell>> &maze, int x, int y)
+{
+    return x > 0 && x < SIZE - 1 && y > 0 && y < SIZE - 1 && !maze[y][x].visited;
+}
 
-void generateMaze(std::vector<std::vector<Cell>>& maze) {
-    // Initialize maze with walls
-    for (int i = 0; i < SIZE; i++) {
-        std::vector<Cell> row;
-        for (int j = 0; j < SIZE; j++) {
-            Cell cell;
-            cell.type = WALL;
-            cell.visited = false;
-            row.push_back(cell);
-        }
-        maze.push_back(row);
-    }
+void GenMaze(vector<vector<Cell>> &maze, int x, int y)
+{
+    maze[y][x].visited = true;
+    maze[y][x].display = (rand() % 100 < WALL_CHANCE * 100) ? '#' : ' ';
 
-    // Generate random entry and exit positions
-    int entryRow = rand() % SIZE;
-    int entryCol = 0;
-    int exitRow = rand() % SIZE;
-    int exitCol = SIZE - 1;
+    vector<pair<int, int>> directions = {{0, 1}, {-1, 0}, {1, 0}};
+    shuffle(directions.begin(), directions.end(), default_random_engine(chrono::system_clock::now().time_since_epoch().count()));
 
-    maze[entryRow][entryCol].type = ENTRY;
-    maze[exitRow][exitCol].type = EXIT;
+    for (const auto &direction : directions)
+    {
+        int a = direction.first * 2, e = direction.second * 2;
+        int b = x + a, d = y + e;
+        int c = x + a / 2, f = y + e / 2;
 
-    // Generate maze paths using recursive backtracking algorithm
-    std::vector<std::pair<int, int>> stack;
-    stack.push_back(std::make_pair(entryRow, entryCol));
-    maze[entryRow][entryCol].visited = true;
-
-    while (!stack.empty()) {
-        int row = stack.back().first;
-        int col = stack.back().second;
-        stack.pop_back();
-
-        std::vector<std::pair<int, int>> neighbors;
-        if (row > 1 && !maze[row - 2][col].visited) {
-            neighbors.push_back(std::make_pair(row - 2, col));
-        }
-        if (row < SIZE - 2 && !maze[row + 2][col].visited) {
-            neighbors.push_back(std::make_pair(row + 2, col));
-        }
-        if (col > 1 && !maze[row][col - 2].visited) {
-            neighbors.push_back(std::make_pair(row, col - 2));
-        }
-        if (col < SIZE - 2 && !maze[row][col + 2].visited) {
-            neighbors.push_back(std::make_pair(row, col + 2));
-        }
-
-        if (!neighbors.empty()) {
-            stack.push_back(std::make_pair(row, col));
-
-            int randomIndex = rand() % neighbors.size();
-            int neighborRow = neighbors[randomIndex].first;
-            int neighborCol = neighbors[randomIndex].second;
-
-            maze[neighborRow][neighborCol].visited = true;
-            maze[neighborRow][neighborCol].type = PATH;
-
-            int wallRow = (row + neighborRow) / 2;
-            int wallCol = (col + neighborCol) / 2;
-            maze[wallRow][wallCol].visited = true;
-            maze[wallRow][wallCol].type = PATH;
-
-            stack.push_back(std::make_pair(neighborRow, neighborCol));
+        if (IsValid(maze, b, d))
+        {
+            maze[f][c].visited = true;
+            maze[f][c].display = (rand() % 100 < WALL_CHANCE * 100) ? '#' : ' ';
+            GenMaze(maze, b, d);
         }
     }
 }
 
-void displayMaze(const std::vector<std::vector<Cell>>& maze) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            switch (maze[i][j].type) {
-                case WALL:
-                    std::cout << "#";
-                    break;
-                case PATH:
-                    std::cout << ".";
-                    break;
-                case ENTRY:
-                    std::cout << "E";
-                    break;
-                case EXIT:
-                    std::cout << "X";
-                    break;
+bool SolveMaze(vector<vector<Cell>> &maze, int x, int y, int prevX, int prevY)
+{
+    if (x < 0 || x >= SIZE || y < 0 || y >= SIZE || maze[y][x].display != ' ')
+        return false;
+    if (y == SIZE - 1)
+    {
+        maze[y][x].display = '.';
+        return true;
+    }
+
+    maze[y][x].display = '.';
+
+    vector<pair<int, int>> moves = {{0, 1}, {-1, 0}, {1, 0}};
+    for (const auto &move : moves)
+    {
+        if (move.first != prevX || move.second != prevY)
+        {
+            if (SolveMaze(maze, x + move.first, y + move.second, -move.first, -move.second))
+                return true;
+        }
+    }
+
+    maze[y][x].display = ' ';
+    return false;
+}
+
+void PrintMaze(const vector<vector<Cell>> &maze)
+{
+    for (const auto &row : maze)
+    {
+        for (const auto &cell : row)
+        {
+            cout << cell.display;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    vector<vector<Cell>> maze(SIZE, vector<Cell>(SIZE));
+    bool solved = false;
+
+    while (!solved)
+    {
+        for (auto &row : maze)
+        {
+            for (auto &cell : row)
+            {
+                cell.visited = false;
+                cell.display = '#';
             }
         }
-        std::cout << std::endl;
+
+        default_random_engine generator(chrono::system_clock::now().time_since_epoch().count());
+        uniform_int_distribution<int> distribution(1, SIZE - 2);
+        int entryX = distribution(generator);
+        maze[0][entryX].display = ' ';
+
+        int exitX = distribution(generator);
+        maze[SIZE - 1][exitX].display = ' ';
+
+        GenMaze(maze, entryX, 1);
+        solved = SolveMaze(maze, entryX, 0, -1, -1);
     }
-}
 
-int main() {
-    srand(time(0));
-
-    std::vector<std::vector<Cell>> maze;
-    generateMaze(maze);
-
-    displayMaze(maze);
+    PrintMaze(maze);
 
     return 0;
 }
